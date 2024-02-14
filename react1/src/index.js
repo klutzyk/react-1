8f31668a5c26b138e7caf04e0d004dbb8f9dfc38
Kulunu Abeysinghe <kabe0009@student.monash.edu>
2024-02-14 13:27:44 +1100

    Init React18 root creation

--- react1/src/index.js
+++ react1/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+const navbar = (
+  <nav>
+    <h1>First navbar</h1>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+    </ul>
+  </nav>
+)
+
+root.render(navbar)
+
+
+// ReactDOM.render(navbar, document.getElementById("root"))
