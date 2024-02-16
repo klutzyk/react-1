3650375a11b78dd90e6365643159a63701e6e582
Kulunu Abeysinghe <kabe0009@student.monash.edu>
2024-02-16 21:22:53 +1100

    return page as function

--- react1/src/index.js
+++ react1/src/index.js
@@ -1,17 +1,19 @@
 import react from "react";
 import ReactDOM from "react-dom";
 
-const page = (
-  <div>
-    <img src="/react1/public/logo192.png" alt="React" />
-    <h1>Fun facts about React!</h1>
-    <ul>
-      <li>Was released in 2013</li>
-      <li>Created by Jordan Walk</li>
-      <li>Over 100k stars on Github</li>
-      <li>Powers thousands of enterprise apps</li>
-    </ul>
-  </div>
-);
+function pageFunc() {
+  return (
+    <div>
+      <img src="/react1/public/logo192.png" alt="React" />
+      <h1>Fun facts about React!</h1>
+      <ul>
+        <li>Was released in 2013</li>
+        <li>Created by Jordan Walk</li>
+        <li>Over 100k stars on Github</li>
+        <li>Powers thousands of enterprise apps</li>
+      </ul>
+    </div>
+  );
+}
 
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(pageFunc(), document.getElementById("root"));
