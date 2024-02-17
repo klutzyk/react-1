fe0aaae8db7450d12434128d67a18ddd5d441acb
Kulunu Abeysinghe <kabe0009@student.monash.edu>
2024-02-17 17:43:34 +1100

    Added component separation and styling

--- react1/src/index.js
+++ react1/src/index.js
@@ -1,15 +1,52 @@
 import react from "react";
 import ReactDOM from "react-dom";
+import "./style.css";
 
-// Component
-function PageComp() {
+// Components
+function Header() {
   return (
-    <div>
+    <header>
+      <title>Hellooo</title>
+      <nav className="nav">
+        <img src={require("./logo.png")} alt="" className="nav-logo" />
+
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>All rights reserved.</p>
+    </footer>
+  );
+}
+
+function MainContent() {
+  return (
+    <body>
+      <h1>Best languages</h1>
       <ol>
         <li>React</li>
         <li>Node</li>
         <li>Angular</li>
       </ol>
+    </body>
+  );
+}
+
+function PageComp() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
